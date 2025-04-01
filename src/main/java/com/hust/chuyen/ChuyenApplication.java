f18210c885f494106609d4e3c07972c4691b1e81
quynhpv <quynhphamvan@tech.admicro.vn>
2025-04-01 20:01:31 +0700

    init config

---
+++ src/main/java/com/hust/chuyen/ChuyenApplication.java
@@ -0,0 +1,13 @@
+package com.hust.chuyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuyenApplication.class, args);
+    }
+
+}
