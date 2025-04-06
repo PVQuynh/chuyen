86aa364f5d225c6e862fc0c35fc2cc0438adff07
quynhpv <quynhphamvan@tech.admicro.vn>
2025-04-06 15:27:21 +0700

    feat: init

---
+++ src/main/java/com/hust/datn/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.hust.datn.controller;
+
+import com.hust.datn.response.MessageResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/index")
+public class IndexController {
+
+    @GetMapping
+    public ResponseEntity<MessageResponse> index() {
+        MessageResponse messageResponse = new MessageResponse();
+        messageResponse.setCode(HttpStatus.OK.value());
+        messageResponse.setMessage(HttpStatus.OK.getReasonPhrase());
+        messageResponse.setData("Đồ án của Chuyên");
+
+        return new ResponseEntity<>(messageResponse, HttpStatus.OK);
+    }
+}
+
